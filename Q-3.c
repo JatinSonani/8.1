93be5c0b7d2a29ff3ed2dd246b1f493e498a49fe
Jatin Sonani <163812641+JatinSonani@users.noreply.github.com>
2024-04-19 10:59:15 +0530

    Update Q-3.c

--- Q-3.c
+++ Q-3.c
@@ -1,27 +1,34 @@
-#include <stdio.h>
+//Q.3 Write a+ Program to perform the addition operation of two 1D arrays & store it in another array. Keep in mind that both array sizes must be the same.
 
-int main() {
-    
-    int size = 5; 
-
-    
-    int A[] = {7, 4, 9, 5, 2};
-    int B[] = {1, 3, 1, 7, 3};
-
-    
-    int result[size];
-
-    
-    for (int i = 0; i < size; ++i) {
-        result[i] = A[i] + B[i];
-    }
-
-    
-    printf("C array after addition:\n");
-    for (int i = 0; i < size; ++i) {
-        printf("%d ", result[i]);
-    }
-    printf("\n");
-
-    
+#include<stdio.h>
+main()
+{
+	int n,i;
+	printf("Enter size of Array = ");
+	scanf("%d",&n);
+	int a[n], b[n],sum[n];
+	printf("Enter A array Value =\n");
+	
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d] = ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	printf("Enter B array Value =\n");
+	
+	for(i=0;i<n;i++)
+	{
+		printf("b[%d] = ",i);
+		scanf("%d",&b[i]);
+	}
+	
+	printf("Sum of A & B array =");
+	
+	for(i=0;i<n;i++)
+	{
+		sum[i] = a[i] + b[i];
+		printf("%d ",sum[i]);
+	}
+	
 }
